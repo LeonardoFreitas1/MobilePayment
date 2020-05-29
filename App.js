ae5c0e9cf23b97b0d5aaa33408f24b600f43756f
LeonardoFreitas1 <leo_paulino2011@hotmail.com>
2020-05-29 09:50:45 -0300

    Formulario Mobile

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Form from './Form'
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Form/>      
     </View>
-  );
+  ); 
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: "100%",
+    height: "100%",
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
